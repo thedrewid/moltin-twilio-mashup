246914c931514bff7fa378fa635f3d5e41b27195
Drew Watson <drew@moltin.com>
2017-10-06 11:49:52 +0100

    export createSMS function as module and add express into packages

---
+++ sms.js
@@ -0,0 +1,17 @@
+// Send an SMS via twilio
+require('dotenv').load();
+
+const accountSid = process.env.TAPI;
+const authToken = process.env.TAUTH;
+// require the Twilio module and create a REST client
+
+const client = require('twilio')(accountSid, authToken);
+
+module.exports.createSMS = (body) => {
+  client.messages.create({
+      to: process.env.PHONE_NUMBER,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      body: body,
+    })
+    .then((message) => console.log(message.sid));
+}
